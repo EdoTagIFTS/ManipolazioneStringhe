4c09acab0ead1d811f30396431f873a4bc0d6743
Edoardo Tagliani <edoardo.tagliani@itsrizzoli.it>
2023-12-19 09:21:34 +0100

    creo funzione toupper

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci una stringa: ");
+        String str = scanner.nextLine();
+
+        scanner.close();
+
+        System.out.println("La stringa contiene " + StringLength(str) + " caratteri");
+
+        System.out.println("La stringa in maiuscolo è: " + ToUpper(str));
+    }
+
+    public static int StringLength(String str){
+        int len = str.length();
+        return len;
+    }
+
+    public static String ToUpper(String str){
+        String str1 = str.toUpperCase();
+        return str1;
     }
 }
